1f3be703966b76e01b422c4215325c50af7959fc
arkanorun <arkanorun@gmail.com>
2023-12-09 12:59:18 -0300

    ajustando config

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,4 +1,6 @@
-import { Router } from 'express'
+const { Router } = require('express')
+const pool = require('../model/connection')
+
 
 const routes = Router()
 
@@ -6,4 +8,9 @@ routes.get('/', (request, response) => {
     return response.json({ message: 'Hello World' })
 })
 
-export default routes
+routes.get('/vacina', async (req, res) => {
+    const vacinacao = await pool.query('select * from vacina')
+    return res.send(vacinacao)
+})
+
+module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,14 @@
-const app = require('./app')
 require('dotenv/config')
+const app = require('./app')
+
+
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Escutando na porta ${process.env.PORT}`)
 })
+
+
+
+
+
