653ba314a92c61b526d7bd570406c956db48dd33
Caroline Nunes <nunescaroline905@gmail.com>
2023-12-09 14:15:59 -0300

    feat: create condig to frontend

---
+++ frontend/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
