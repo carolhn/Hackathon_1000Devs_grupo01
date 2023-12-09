b5babdb231766bded1ca50b307a1535373705dd3
Caroline Nunes <nunescaroline905@gmail.com>
2023-12-09 15:14:19 -0300

    fix: adjusting config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
-        <Route path="/" />
+        <Route element={<Home />} path="/" />
       </Routes>
     </div>
   )
