ed9f4fb2aa817c22d9cfc4a3659716cdb90d208c
Caroline Nunes <nunescaroline905@gmail.com>
2023-12-09 18:12:19 -0300

    fix: adjusting css and page home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import { Home } from './pages/Home'
 import { Header } from './components/Header'
 import './App.css'
 import { Footer } from './components/Footer'
