47631ba1c2185e1e5ca48d76f946c3dbc4108d77
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-12-09 21:18:07 -0300

    feat: vacine per period

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,14 +1,15 @@
 const { Router } = require('express')
 const {createPacientController, getAllPacientscontroller, updatePacientController, getPacientByIdController} = require('../controller/patient.controller')
+const { getVacineByPeriodController } = require('../controller/vacina.controller')
 const routes = Router()
-
-//Get all pacients
+routes.get('/vacinasporperiodo',getVacineByPeriodController)
 routes.get('/paciente',getAllPacientscontroller)
-//CREATE Patient
+
 routes.post("/paciente",createPacientController) 
-//Update Patient
+
 routes.patch("/paciente/:id",updatePacientController) 
-//Get patient by id
+
 routes.get('/paciente/:id',getPacientByIdController)
+
 
 module.exports = routes
